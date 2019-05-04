ad7f2c8995e8867ad0f9b814378eaf7450b29167
Derrick Hau <derrickhau@gmail.com>
2019-05-04 09:57:24 +0000

    Move key generation off chain

--- Contracts/Remittance.sol
+++ Contracts/Remittance.sol
@@ -5,35 +5,32 @@ import "./Pausable.sol";
 contract Remittance is Pausable {
 
     mapping(address => uint) public balances;
-    address remittanceAddress;
+    address carolAddress;
     bytes32 keyHash;
 
-    event LogRemittanceCreated(address sender, uint amount, address receiver);
     event LogWithdrawal(address indexed receiver, uint amount);
     event LogKillExecuted(address owner, uint refund);
-    
-    constructor (uint remittanceKey, uint recipientKey, address _remittanceAddress) 
-        public payable onlyOwner() {
-        require(_remittanceAddress != address(0), "Invalid address");
-        remittanceAddress = _remittanceAddress;
-        keyHash = keccak256(abi.encodePacked(remittanceKey, recipientKey, _remittanceAddress, msg.value));
-        balances[remittanceAddress] = msg.value;
-        emit LogRemittanceCreated(msg.sender, msg.value, remittanceAddress);
+
+    // keyHash generated offchain
+    constructor (bytes32 _keyHash, address _carolAddress) public payable {
+        carolAddress = _carolAddress;
+        keyHash = _keyHash;
+        balances[carolAddress] = msg.value;
     }
     
-    function withdrawFunds (uint remittanceKey, uint recipientKey) public notPaused() {
-        require(keccak256(abi.encodePacked(remittanceKey, recipientKey, msg.sender, balances[msg.sender])) == keyHash, "Access denied"); 
+    function withdrawFunds (uint carolKey, uint bobKey) public notPaused() {
+        require(keccak256(abi.encodePacked(carolKey, bobKey)) == keyHash, "Access denied"); 
         uint amountDue = balances[msg.sender];
         require(amountDue > 0, "Insufficient funds");
         balances[msg.sender] = 0;
-        emit LogWithdrawal(msg.sender, amountDue);
         msg.sender.transfer(amountDue);
+        emit LogWithdrawal(msg.sender, amountDue);
     }
 
     function kill () public onlyOwner {
-        uint refund = balances[remittanceAddress];
+        uint refund = balances[carolAddress];
         require(refund > 0, "No funds available");
-        balances[remittanceAddress] = 0;
+        balances[carolAddress] = 0;
         emit LogKillExecuted(msg.sender, refund);
         msg.sender.transfer(refund);
     }
