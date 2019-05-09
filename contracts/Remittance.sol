187fa95da6b0fb98190b8aa38ce1f2766ab446d0
Derrick Hau <derrickhau@gmail.com>
2019-05-09 16:17:10 +0000

    Update keyHash to public, add msg.sender to keyHash

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -4,20 +4,20 @@ import "./Pausable.sol";
 
 contract Remittance is Pausable {
 
-    bytes32 keyHash;
+    bytes32 public keyHash;
     uint blockExpiration;
 
     event LogWithdrawal(address indexed receiver, uint amount);
     event LogKillExecuted(address owner, uint refund);
 
     // keyHash generated offchain
-    constructor (bytes32 _keyHash, uint daysValid) public payable {
+    constructor (bytes32 _keyHash, uint secondsValid) public payable {
         keyHash = _keyHash;
-        blockExpiration = block.number + (daysValid * 86400 / 15);
+        blockExpiration = block.number + (secondsValid / 15);
     }
     
     function withdrawFunds (uint carolKey, uint bobKey) public notPaused() {
-        require(keccak256(abi.encodePacked(carolKey, bobKey)) == keyHash, "Access denied"); 
+        require(keccak256(abi.encodePacked(carolKey, bobKey, msg.sender)) == keyHash, "Access denied"); 
         require(block.number < blockExpiration);
         emit LogWithdrawal(msg.sender, address(this).balance);
         msg.sender.transfer(address(this).balance);
