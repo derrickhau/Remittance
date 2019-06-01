55d82309440fc3280f1b07dc5c9999d1b913eea2
Derrick <derrickhau@gmail.com>
2019-06-01 10:27:15 +0700

    Update Ownable.sol with claimOwnership

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -2,8 +2,10 @@ pragma solidity ^0.5.0;
 
 contract Ownable {
     address private owner;
+    address private newOwner;
     
-    event LogTransferOwnership (address indexed previousOwner, address indexed newOwner);
+    event LogNominateNewOwner (address indexed previousOwner, address indexed newOwner);
+    event LogClaimOwnership (address indexed previousOwner, address indexed newOwner);
     event LogRenounceOwnership (address indexed owner);
 
     modifier onlyOwner() {
@@ -17,9 +19,16 @@ contract Ownable {
     
     function getOwner() public view returns (address) { return owner; }
     
-    function transferOwnership(address newOwner) public onlyOwner {
+    function nominateNewOwner(address _newOwner) public onlyOwner {
         require(newOwner != address(0), "Invalid address");
-        emit LogTransferOwnership (owner, newOwner);
+        emit LogNominateNewOwner (owner, newOwner);
+        newOwner = _newOwner;
+    }
+    
+    function claimOwnership() public {
+        require(msg.sender != address(0), "Invalid address");
+        require(msg.sender == newOwner, "Access denied");
+        emit LogClaimOwnership (owner, newOwner);
         owner = newOwner;
     }
     
